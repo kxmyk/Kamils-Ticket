c14964400e29a616530ebcdb700fe6c52df6a654
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-10 22:32:15 +0200

    implemented user login logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 // @desc Register a new user
 // @route /api/users
@@ -37,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 			_id: user._id,
 			name: user.name,
 			email: user.email,
+			token: generateToken(user._id)
 		})
 	} else {
 		res.status(400)
@@ -48,8 +50,30 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route /api/users/login
 // @access public
 const loginUser = asyncHandler( async (req, res) => {
-	res.send('Login Route')
+	const {email, password} = req.body
+
+	const user = await User.findOne({email})
+
+	// Check if user and password match
+	if (user && (await bcrypt.compare(password, user.password))) {
+		res.status(200).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user._id)
+		})
+	} else {
+		res.status(401)
+		throw new Error('Invalid credentials')
+	}
 })
+
+// Generate jwt token
+const generateToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: '30d',
+	})
+}
 
 module.exports = {
 	registerUser,
