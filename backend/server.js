b4d82a83584d4cb9ea6d122d988b6e3228771099
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-17 22:08:01 +0200

    deployment preparation

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('/Users/kamillukasiuk/Desktop/webDev/kamils_ticket/backend/middleware/errorMiddleware.js')
 const connectDB = require('./config/db')
@@ -13,15 +14,23 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-
-app.get('/', (req, res) => {
-	res.status(200).json({message: '( ๑‾̀◡‾́)σ\"'})
-})
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/ticket', require('./routes/ticketRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({message: 'Welcome to Kamils ticket ( ๑‾̀◡‾́)σ" '})
+	})
+}
 
 app.use(errorHandler)
 
