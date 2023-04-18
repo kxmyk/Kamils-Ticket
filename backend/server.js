f18f4efba777b3dc80319ceb66fe6858776ce5c6
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-18 19:41:25 +0200

    deployment preparation pt5

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,18 @@ app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 // Serve Frontend
-if (process.env.NODE_ENV === 'production') {
-	// Set build folder as static
-	app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-	app.get('*', (req, res) =>
-		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-	)
-} else {
-	app.get('/', (req, res) => {
-		res.status(200).json({message: 'Welcome to Kamils ticket ( ๑‾̀◡‾́)σ" '})
-	})
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	// Set build folder as static
+// 	app.use(express.static(path.join(__dirname, '../frontend/build')))
+//
+// 	app.get('*', (req, res) =>
+// 		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+// 	)
+// } else {
+// 	app.get('/', (req, res) => {
+// 		res.status(200).json({message: 'Welcome to Kamils ticket ( ๑‾̀◡‾́)σ" '})
+// 	})
+// }
 
 app.use(errorHandler)
 
