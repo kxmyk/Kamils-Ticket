b409c8ac1892782bba32ef3cecdafbdb92e1c606
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-18 19:50:01 +0200

    deployment preparation pt8

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-Serve Frontend
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
 	// Set build folder as static
 	app.use(express.static(path.join(__dirname, '../frontend/build')))
