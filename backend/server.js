5da4d7296f7ec97ce6a6cfcf04f4a21c485e8fbe
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-18 19:49:05 +0200

    deployment preparation pt7

--- backend/server.js
+++ backend/server.js
@@ -18,20 +18,20 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// Serve Frontend
-// if (process.env.NODE_ENV === 'production') {
-// 	// Set build folder as static
-// 	app.use(express.static(path.join(__dirname, '../frontend/build')))
-//
-// 	app.get('*', (req, res) =>
-// 		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-// 	)
-// } else {
-// 	app.get('/', (req, res) => {
-// 		res.status(200).json({message: 'Welcome to Kamils ticket ( ๑‾̀◡‾́)σ" '})
-// 	})
-// }
+Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	// Set build folder as static
+	app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+	)
+} else {
+	app.get('/', (req, res) => {
+		res.status(200).json({message: 'Welcome to Kamils ticket ( ๑‾̀◡‾́)σ" '})
+	})
+}
 
 app.use(errorHandler)
 
-app.listen(PORT)
+app.listen(PORT, () => 'works :')
