21a666ccbd16b7ae5e29510bdd59e72514d47a6f
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-19 16:51:22 +0200

    bug fixes pt3

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
 	// Set build folder as static
-	app.use(express.static(path.join(__dirname, '../frontend/build')))
+	app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 	app.get('*', (_, res) => {
-		res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+		res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 	})
 } else {
 	app.get('/', (_, res) => {
